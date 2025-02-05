99ebdc552d15ded7dd2b384dc1e9b4804051a872
keyansheng <65121402+keyansheng@users.noreply.github.com>
2025-02-05 22:30:28 +0800

    Add dialogBox to Main

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -3,6 +3,7 @@ import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ScrollPane;
 import javafx.scene.control.TextField;
+import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
@@ -13,10 +14,15 @@ public class Main extends Application {
     private TextField userInput = new TextField();
     private Button sendButton = new Button("Send");
     private Scene scene;
+    private Image userImage = new Image(this.getClass().getResourceAsStream("/images/DaUser.png"));
+    private Image dukeImage = new Image(this.getClass().getResourceAsStream("/images/DaDuke.png"));
 
     @Override
     public void start(Stage stage) {
         scrollPane.setContent(dialogContainer);
+
+        DialogBox dialogBox = new DialogBox("Hello!", userImage);
+        dialogContainer.getChildren().addAll(dialogBox);
 
         AnchorPane mainLayout = new AnchorPane();
         mainLayout.getChildren().addAll(scrollPane, userInput, sendButton);
