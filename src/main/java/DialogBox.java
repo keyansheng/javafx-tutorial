2396f8719351268c98978c380b7d3a74f1a1eb4c
keyansheng <65121402+keyansheng@users.noreply.github.com>
2025-02-05 22:49:53 +0800

    Add styles to DialogBox

--- src/main/java/DialogBox.java
+++ src/main/java/DialogBox.java
@@ -1,3 +1,4 @@
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
@@ -10,6 +11,12 @@ public class DialogBox extends HBox {
     public DialogBox(String s, Image i) {
         text = new Label(s);
         displayPicture = new ImageView(i);
+
+        text.setWrapText(true);
+        displayPicture.setFitWidth(100.0);
+        displayPicture.setFitHeight(100.0);
+        this.setAlignment(Pos.TOP_RIGHT);
+
         this.getChildren().addAll(text, displayPicture);
     }
 }
